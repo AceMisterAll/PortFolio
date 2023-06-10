447d0e3f9a1f03b7a54c505056e53dcec18d50b7
Allan Chiron <allanloannchiron@gmail.com>
2023-06-10 13:30:37 +0200

    page stage pubert

--- src/Controller/StageController.php
+++ src/Controller/StageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StageController extends AbstractController
@@ -22,10 +23,34 @@ class StageController extends AbstractController
         ]);
     }
 
-    #[Route('/stage/pubert', name: 'stagekline')]
+    #[Route('/stage/kline', name: 'stagekline')]
     public function kline(): Response
     {
         return $this->render('stage/kline.html.twig', [
         ]);
+    }
+
+    #[Route('/docinstallation', name: 'docinstallation')]
+    public function docinstallation(): Response
+    {
+        $pdfcv = $this->getParameter('kernel.project_dir').'/public/pdf/Procedure_installation_screenhub.pdf';
+        return
+            $this->file($pdfcv, 'Procedure_installation_screenhub.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+
+    #[Route('/docutilisation', name: 'docutilisation')]
+    public function docutilisation(): Response
+    {
+        $pdfcv = $this->getParameter('kernel.project_dir').'/public/pdf/Manuel_utilisation_screenhub.pdf';
+        return
+            $this->file($pdfcv, 'Manuel_utilisation_screenhub.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+
+    #[Route('/docpresentation', name: 'docpresentation')]
+    public function ddocpresentation(): Response
+    {
+        $pdfcv = $this->getParameter('kernel.project_dir').'/public/pdf/Procedure_installation_screenhub.pdf';
+        return
+            $this->file($pdfcv, 'Procedure_installation_screenhub.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
